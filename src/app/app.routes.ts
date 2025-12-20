91adb87bb2b61fb7bc26bfe640b2bbdb0860fc51
FernanJS17 <109115569+FernanJS17@users.noreply.github.com>
2025-12-20 16:02:48 -0600

    feat(initialcommit)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
+import { HomeComponent } from './core/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+        {path: '', component: HomeComponent},
+    ]
+}];
